f8dce8b9b6f55f9612fb70c11f1e1445ea788e37
RattyGirl <chloeholmes8192@gmail.com>
2023-05-19 18:07:18 +1000

    readers

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,173 @@
-use std::{path::PathBuf, fs::{self, File}, io::BufReader};
+use std::io::{BufRead, Read};
+use std::{
+    fs::{self, File},
+    io::BufReader,
+    path::PathBuf,
+};
 
-
+#[derive(Debug, Clone)]
 pub struct BLT_Format {
-    can_count: i32,
-    vacancies: i32,
-    withdraws: Vec<i32>,
+    can_count: u32,
+    vacancies: u32,
+    withdraws: Vec<u32>,
     votes: Vec<Vote>,
     candidate_names: Vec<String>,
-    election_name: String
+    election_name: String,
 }
 
-impl BLT_Format {
-    pub fn read_from_file(file: PathBuf) {
-        let file: File = File::open(file).expect("Could not open file");
-        let mut buf_reader = BufReader::new(file);
-        if buf_reader.fill_buf()?.len() > 0 {
-            assert!(!buf_reader.buffer().is_empty());
+impl From<GoogleSheetFormat> for BLT_Format {
+    fn from(value: GoogleSheetFormat) -> Self {
+        Self {
+            can_count: value.can_count,
+            vacancies: value.vacancies,
+            withdraws: Vec::new(),
+            votes: value.votes,
+            candidate_names: value.candidate_names,
+            election_name: "".to_string(),
         }
     }
 }
 
+impl BLT_Format {
+    pub fn read_from_file(file: PathBuf) -> Self {
+        let file = fs::read_to_string(file).unwrap();
+        //assumes correct format
+        let mut lines = file.lines();
+        let (can_count, vacancies) = lines.next().unwrap().trim_end().split_once(' ').unwrap();
+        let can_count = can_count.parse().unwrap();
+        let vacancies = vacancies.parse().unwrap();
+
+        let mut votes: Vec<Vote> = Vec::new();
+        let mut withdraws: Vec<u32> = Vec::new();
+        let mut inballot = true;
+        let mut candidates: Vec<String> = Vec::new();
+        for line in lines {
+            if line.trim().eq("0") {
+                inballot = false;
+                continue;
+            }
+            if line.starts_with('-') {
+                withdraws = line
+                    .split(" ")
+                    .map(|x| x.replace("-", "").parse().unwrap())
+                    .collect();
+                continue;
+            }
+            if inballot {
+                let mut prefs = line.trim().split(" ");
+                let count: i32 = prefs.next().unwrap().parse().unwrap();
+                let mut ballot: Vec<u32> = Vec::new();
+                for pref in prefs {
+                    if pref.trim().eq("0") {
+                        break;
+                    }
+                    ballot.push(pref.parse::<u32>().unwrap());
+                }
+                votes.push(Vote {
+                    count,
+                    preferences: ballot,
+                });
+            } else {
+                candidates.push(line.trim().replace("\"", "").to_string());
+            }
+        }
+
+        Self {
+            can_count,
+            vacancies,
+            withdraws,
+            votes,
+            candidate_names: candidates,
+            election_name: "".to_string(),
+        }
+    }
+
+    pub fn cleanup(&mut self) {
+        self.votes.iter_mut().for_each(|x| {
+            x.clean();
+        });
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Vote {
-    string: String
+    count: i32,
+    preferences: Vec<u32>,
+}
+
+impl Vote {
+    pub fn clean(&mut self) {
+        self.preferences = self
+            .preferences
+            .clone()
+            .into_iter()
+            .filter(|x| x != &(0 as u32))
+            .collect();
+    }
+}
+
+#[derive(Debug)]
+pub struct GoogleSheetFormat {
+    can_count: u32,
+    vacancies: u32,
+    votes: Vec<Vote>,
+    candidate_names: Vec<String>,
+}
+
+impl GoogleSheetFormat {
+    pub fn read_from_file(file: PathBuf) -> Self {
+        let file = fs::read_to_string(file).unwrap();
+        //assumes correct format
+        let mut lines = file.lines();
+        let (can_count, vacancies) = lines.next().unwrap().trim_end().split_once(' ').unwrap();
+        let can_count: u32 = can_count.parse().unwrap();
+        let vacancies = vacancies.parse().unwrap();
+
+        let mut votes: Vec<Vote> = Vec::new();
+        let mut withdraws: Vec<i32> = Vec::new();
+        let mut inballot = true;
+        let mut candidates: Vec<String> = Vec::new();
+        for line in lines {
+            if line.trim().eq("0") {
+                inballot = false;
+                continue;
+            }
+            if line.starts_with('-') {
+                withdraws = line
+                    .split(" ")
+                    .map(|x| x.replace("-", "").parse().unwrap())
+                    .collect();
+                continue;
+            }
+            if inballot {
+                let mut prefs = line.trim().split("	");
+                let count: i32 = 1;
+                let mut ballot: Vec<u32> = vec![0; can_count as usize];
+
+                for (x, pref) in prefs.enumerate() {
+                    if pref.trim().eq("0") {
+                        break;
+                    }
+                    if pref.ne("") {
+                        let position: usize = pref.parse().unwrap();
+                        ballot[position - 1] = (x + 1) as u32;
+                    }
+                }
+
+                votes.push(Vote {
+                    count,
+                    preferences: ballot,
+                });
+            } else {
+                candidates.push(line.trim().replace("\"", "").to_string())
+            }
+        }
+
+        Self {
+            can_count,
+            vacancies,
+            votes,
+            candidate_names: candidates,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
-use election_counter::BLT_Format;
+use election_counter::{BLT_Format, GoogleSheetFormat};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(value_name = "FILE")]
+    #[arg(value_name = "Ballot file")]
     file: PathBuf,
-    
+
     #[arg(long)]
     sheets: bool,
 }
@@ -16,5 +16,14 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
 
-    let blt_info = BLT_Format::read_from_file(cli.file);
+    let mut blt_info = match cli.sheets {
+        true => BLT_Format::from(GoogleSheetFormat::read_from_file(cli.file)),
+        false => BLT_Format::read_from_file(cli.file),
+    };
+    blt_info.cleanup();
+    stv(blt_info.clone());
+}
+
+fn stv(blt: BLT_Format) {
+
 }
